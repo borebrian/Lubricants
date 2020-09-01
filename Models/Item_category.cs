6816b32a9bd0d1ac6cb4954a91d058e382a30c90
Bore Brian <borebrian77@gmail.com>
2020-08-31 22:42:11 -0700

    Add project files.

---
+++ Models/Item_category.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lubricants.Models
+{
+    public class Item_category
+    {
+        [Key]
+        [Required]
+
+        [Display(Name = "ID", Prompt = "ID")]
+        public int ID { get; set; }
+
+
+        [Display(Name = "Category name:", Prompt = "Category name")]
+        [Required]
+        [MaxLength(50)]
+        public string Status { get; set; }
+
+
+
+        [Display(Name = "Upload Image:", Prompt = "Upload image")]
+        [Required(ErrorMessage = "Please choose profile image")]
+        public string Category_item { get; set; }
+    }
+}
